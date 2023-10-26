ec534ebd7c7d13b84cf708a5a4b329b3608c0cee
MAC PRO <macpro@MacBook-Pro-de-MAC.local>
2023-10-26 11:20:51 -0600

    Implemented add_item method in Label class

--- classes/label.rb
+++ classes/label.rb
@@ -4,9 +4,15 @@ class Label
   def initialize(title, color)
     @title = title
     @color = color
+    @items = []
   end
 
   def to_s
     "Title: #{@title}, Color: #{@color}"
   end
+
+  def add_item(item)
+    @items << item
+    item.label = self
+  end  
 end
